fcb5241cb2120aac1354860e17b30711a05f1132
Manasa <64478792+maradamanasa@users.noreply.github.com>
2020-07-21 22:00:36 +0530

    Add files via upload

---
+++ assignment day 2.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("hello world")
+
+
+# In[3]:
+
+
+print(""" hello python""")
+
+
+# In[4]:
+
+
+print("hello\tworld")
+
+
+# In[9]:
+
+
+name="manasa"
+age=20
+marks=95
+print("the name is %s marks is %4.2f age is %d"%(name,marks,age))
+
+
+# In[10]:
+
+
+a=10
+a
+
+
+# In[11]:
+
+
+_10=20
+_10
+
+
+# In[12]:
+
+
+id(_10)
+
+
+# In[13]:
+
+
+b=10
+a==b
+
+
+# In[14]:
+
+
+a is b
+
+
+# In[16]:
+
+
+a=456
+b=768
+a is b
+
+
+# # operators in python 
+# 
+
+# # Arithmetic operators
+# 
+
+# In[17]:
+
+
+a=30
+b=20
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+
+# # comparison operators
+
+# always returns boolean values
+
+# In[18]:
+
+
+a=20
+b=15
+a>b
+
+
+# In[19]:
+
+
+a<b
+
+
+# In[20]:
+
+
+a>=b
+
+
+# In[21]:
+
+
+a<=b
+
+
+# In[22]:
+
+
+a!=b
+
+
+# In[23]:
+
+
+x=y=5
+x==y
+
+
+# In[24]:
+
+
+x!=y
+
+
+# # Assignment operators
+
+# In[27]:
+
+
+a=10
+b=20
+a+=5
+a
+
+
+# In[28]:
+
+
+b-=10
+b
+
+
+# In[29]:
+
+
+b*=5
+b
+
+
+# In[30]:
+
+
+a/=5
+a
+
+
+# In[31]:
+
+
+a**=3
+a
+
+
+# # Bitwise operators
+
+# In[32]:
+
+
+a=10
+b=24
+a|b
+
+
+# In[33]:
+
+
+a&b
+
+
+# In[34]:
+
+
+~a
+
+
+# In[35]:
+
+
+a^b
+
+
+# # Membership operator
+# 
+
+# In[38]:
+
+
+x="manasa"
+'a' in x
+
+
+# In[39]:
+
+
+'p' in x
+
+
+# In[40]:
+
+
+'i' not in x
+
+
+# # Identity operators
+
+# In[41]:
+
+
+a=10
+b=10
+a is b
+
+
+# In[42]:
+
+
+a is not b
+
+
+# In[43]:
+
+
+10+20/15*4
+
+
+# In[ ]:
+
+
+
+
